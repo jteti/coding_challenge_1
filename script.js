7760511607f2b45b5df4dcb82db51b4ff33dbfec
jteti <mr.jteti@gmail.com>
2019-03-29 18:03:04 -0400

    Console 2 values

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var BMI, massJohn, heightJohn, massMark, heightMark, markHigherBMI;
+
 massJohn = 92;
 heightJohn = 1.95;
+
 massMark = 78;
 heightMark = 1.69;
+
 johnBMI = massJohn / (heightJohn * heightJohn);
 markBMI = massMark / (heightMark * heightMark);
+console.log(markBMI, johnBMI)
+
 markHigherBMI = markBMI > johnBMI;
-console.log("John's BMI is " + johnBMI);
-console.log("Mark's BMI is " + markBMI);
 console.log("Is Mark's BMI less than John's? " + markHigherBMI);
